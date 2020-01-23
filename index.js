cd140f858041e7c002df9838fe8fa19650519452
christiansallen <christiansallen@gmail.com>
2020-01-22 22:43:19 -0700

    finished the code challenge

--- index.js
+++ index.js
@@ -1,64 +1,60 @@
 let created = false;
-let body = document.getElementById("container");
+//Need to reference the container div in order to insert the table later
+let container = document.getElementById("container");
 let resetAlert = document.createElement("p");
 let resetText = document.createTextNode(
   "You need to reset the table before creating a new one"
 );
 let numberAlert = document.createElement("p");
 let numberText = document.createTextNode(
-  "Rows and Columns can't be less than 0 or greater than 6 or a decimal."
+  "Rows and Columns can't be less than 0, greater than 6, or a decimal."
 );
-let resetAlertCount = false;
-let numberAlertCount = false;
+let resetAlertBool = false;
+let numberAlertBool = false;
 
-function makeResetButton() {
-  //Create a reset button and append to btn-container
-  let resetButton = document.createElement("button");
-  let buttonText = document.createTextNode("Reset");
-  resetButton.appendChild(buttonText);
-  btnContainer.appendChild(resetButton);
-
-  //Remove table and change created to false
-  resetButton.onclick = function(e) {
-    e.preventDefault();
-    body.removeChild(tbl);
-    btnContainer.removeChild(resetButton);
-    if (resetAlertCount) {
-      body.removeChild(resetAlert);
-    }
-    if (numberAlertCount) {
-      body.removeChild(numberAlert);
-    }
-    created = false;
-  };
+//CHECK NUMBERS FUNCTION
+function checkNumbers(rows, columns) {
+  if (rows < 0 || rows > 6 || rows % 1 != 0) {
+    numberAlert.appendChild(numberText);
+    numberAlert.setAttribute("class", "alert");
+    container.appendChild(numberAlert);
+    numberAlertBool = true;
+    return 0;
+  } else if (columns < 0 || columns > 6 || columns % 1 != 0) {
+    numberAlert.appendChild(numberText);
+    numberAlert.setAttribute("class", "alert");
+    container.appendChild(numberAlert);
+    numberAlertBool = true;
+    return 0;
+  } else {
+    // console.log("check numbers passed");
+    return 1;
+  }
 }
 
+//CREATE TABLE FUNCTION
 function createTable(e) {
   e.preventDefault();
   //If nothing is in rows or columns, default value will be 6.
+  let tbl = document.createElement("table");
+  let tblBody = document.createElement("tbody");
   const rows = document.getElementById("rows").value || 6;
   const columns = document.getElementById("columns").value || 6;
-  let btnContainer = document.getElementById("buttons-container");
 
   if (!created) {
-    if (checkNumbers(rows, columns) === 0) {
-      let resetButton = document.createElement("button");
-      let buttonText = document.createTextNode("Reset");
-      resetButton.appendChild(buttonText);
-      btnContainer.appendChild(resetButton);
+    // toggle created to true
+    created = true;
 
+    //Dont create a table if there are invalid numbers
+    if (checkNumbers(rows, columns) === 0) {
       return;
     }
-    console.log("past checknumbers function");
-
-    //Need to reference the container div in order to insert the table later
-    let tbl = document.createElement("table");
-    let tblBody = document.createElement("tbody");
 
     // creating all rows
     for (let i = 1; i <= rows; i++) {
       // creates a table row
       let row = document.createElement("tr");
+      row.setAttribute("class", "multiplication-row");
 
       //creating the columns with text in them
       for (let j = 1; j <= columns; j++) {
@@ -78,36 +74,43 @@ function createTable(e) {
 
     // put the <tbody> in the <table>
     tbl.appendChild(tblBody);
-    tbl.setAttribute("class", "multiplication-table");
+    tbl.setAttribute("class", "multiplication-table-js");
+    tbl.setAttribute("id", "new-table");
     // put the <table> into container div
-    body.appendChild(tbl);
-    // increment count
-    created = true;
-    makeResetButton();
+    container.appendChild(tbl);
   } else {
+    //Dont create another error if there's already one present
+    if (resetAlertBool) {
+      //   console.log("already has a reset alert");
+      return;
+    }
     resetAlert.appendChild(resetText);
     resetAlert.setAttribute("class", "alert");
-    body.appendChild(resetAlert);
-    resetAlertCount = true;
+    container.appendChild(resetAlert);
+    resetAlertBool = true;
     return;
   }
 }
 
-function checkNumbers(rows, columns) {
-  if (rows < 0 || rows > 6 || rows % 1 != 0) {
-    console.log("row error");
-    numberAlert.appendChild(numberText);
-    numberAlert.setAttribute("class", "alert");
-    body.appendChild(numberAlert);
-    numberAlertCount = true;
-    return 0;
-  } else if (columns < 0 || columns > 6 || columns % 1 != 0) {
-    numberAlert.appendChild(numberText);
-    numberAlert.setAttribute("class", "alert");
-    body.appendChild(numberAlert);
-    numberAlertCount = true;
-    return 0;
-  } else {
-    return;
+//RESET TABLE FUNCTION
+function resetTable(e) {
+  e.preventDefault();
+  //Need to create this so I can delete a temporary table. Without this, the tables will keep appending onto each other even after 'deletion'.
+  let tempTable = document.getElementById("new-table");
+
+  //Remove table and change created to false
+  if (container.contains(tempTable)) {
+    container.removeChild(tempTable);
   }
+  if (resetAlertBool) {
+    container.removeChild(resetAlert);
+    resetAlertBool = false;
+  }
+  if (numberAlertBool) {
+    container.removeChild(numberAlert);
+    numberAlertBool = false;
+  }
+  document.getElementById("rows").value = null;
+  document.getElementById("columns").value = null;
+  created = false;
 }
